01d74e371a1010d805914625868155afe1d1521b
Alex Knight <alex@alexknight.co.uk>
2023-10-27 18:24:45 +0100

    Get Vite to include

--- vttModule/vite.config.js
+++ vttModule/vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from "vite";
+import { resolve } from 'path'
 import scalaJSPlugin from "@scala-js/vite-plugin-scalajs";
 
 export default defineConfig({
     plugins: [scalaJSPlugin()],
+    build: {
+        lib: {
+            entry: resolve(__dirname, "main.js"),
+            name: "VTT Module"
+        }
+    }
 });
