7763d39e1a8db607a4ce9131f88d637988894f6e
Alex Knight <alex@alexknight.co.uk>
2023-10-27 09:22:41 +0100

    Unrelated: Import module.json with generated JS files

--- vttModule/src/main/scala/scripts/AlexsDungeon.scala
+++ vttModule/src/main/scala/scripts/AlexsDungeon.scala
@@ -2,8 +2,12 @@ package scripts
 
 import uk.co.alexknight.foundryTypes.Hooks
 
-import scala.scalajs.js.annotation.JSGlobalScope
+import scala.scalajs.js.annotation.{JSGlobalScope, JSImport}
 import scalajs.*
+
+@js.native @JSImport("/module.json", JSImport.Default)
+val moduleJson: String = js.native
+
 object AlexsDungeon {
   def main(args: Array[String]): Unit = {
     println("Hello world!")
